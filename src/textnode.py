be4598e419b1cdbc68fc8c7eb759a9d526413635
poupardm-GhostWrath <mathpoupard@gmail.com>
2026-02-21 10:50:27 -0700

    TextNode Lesson

---
+++ src/textnode.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN_TEXT = "text (plain)"
+    BOLD_TEXT = "**Bold text**"
+    ITALIC_TEXT = "_Italic text_"
+    CODE_TEXT = "'Code text'"
+    LINK_TEXT = "[anchor text](url)"
+    IMAGE = "![alt text](url)"
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        return (
+            self.text == other.text and 
+            self.text_type == other.text_type and 
+            self.url == other.url)
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
