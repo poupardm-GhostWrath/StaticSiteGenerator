7b039d39f23c6a07c3e76bfb0969d4da763876bc
poupardm-GhostWrath <mathpoupard@gmail.com>
2026-02-23 13:59:11 -0700

    Block Types Lesson

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,14 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     document = []
     blocks = markdown.split("\n\n")
@@ -6,3 +17,16 @@ def markdown_to_blocks(markdown):
             continue
         document.append(block.strip())
     return document
+
+def block_to_block_type(text):
+    if len(re.findall(r"\#{1,6} (.*)", text)) == 1:
+        return BlockType.HEADING
+    if len(re.findall(r"\`{3}\n(^[\s\S]+$)\n\`{3}", text, re.MULTILINE)) == 1:
+        return BlockType.CODE
+    if len(re.findall(r"\> {0,1}(.*)", text)) == 1:
+        return BlockType.QUOTE
+    if len(re.findall(r"\- (.*)", text)) == 1:
+        return BlockType.UNORDERED_LIST
+    if len(re.findall(r"\d{1,}. (.*)", text)) == 1:
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestMarkdownToHTML(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -44,6 +44,42 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_type(self):
+        md = """
+### Heading 3
+
+```
+{
+    Code Blocks
+    Code Blocks
+}
+```
+
+> Quotes 
+
+- Unordered Item 1
+
+1. Ordered Item 1
+
+Just Paragraph
+"""
+        result = [
+            BlockType.HEADING,
+            BlockType.CODE,
+            BlockType.QUOTE,
+            BlockType.UNORDERED_LIST,
+            BlockType.ORDERED_LIST,
+            BlockType.PARAGRAPH
+        ]
+        blocks = markdown_to_blocks(md)
+        counter = 0
+        for block in blocks:
+            block_type = block_to_block_type(block)
+            self.assertEqual(block_type, result[counter])
+            counter += 1
+
+
 
 
 if __name__ == "__main__":
